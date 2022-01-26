c94a3e3909c121687f72480124d41dbef98af434
ubcDanica <danica5471@gmail.com>
2022-01-25 16:57:50 -0800

    Add test cases

---
+++ Minepump/Mine.m
@@ -0,0 +1,10 @@
+RunningExample: [STARTCOMMAND___] [STOPCOMMAND___] [METHANEALARM___] [METHANEQUERY___] [LOWWATERSENSOR___] [HIGHWATERSENSOR___] BASE___ :: Program;
+
+
+	
+	
+	
+	
+	
+	
+	
---
+++ xstream/XstreamExample.m
@@ -0,0 +1,2 @@
+//hared
+RunningExample : [CLASS_ALIAS___] [FIELD_ALIAS___] [OMIT_FIELD___] [IMPLICIT_ARRAY___] [ATTRIBUTES___] [BOOLEAN_CONVERTER___] [TREE_STRUCTURE___] BASE___ :: Program;
