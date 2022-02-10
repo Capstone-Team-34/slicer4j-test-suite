5673b6cb7af2efdd561139ff68a8c1e42681dd21
ubcDanica <danica5471@gmail.com>
2022-02-09 16:56:43 -0800

    Test dummy

--- Jtopas/src/test/java/JtopasTest/Test.java
+++ Jtopas/src/test/java/JtopasTest/Test.java
@@ -6,8 +6,8 @@ import util.Token;
 public class Test {
     public static void main(String args[])
     {
-        TokenizerSource T = new InputStreamSource();
-        PluginTokenizer PT = new PluginTokenizer(T);
+//        TokenizerSource T = new InputStreamSource();
+//        PluginTokenizer PT = new PluginTokenizer(T);
     }
 }
 
