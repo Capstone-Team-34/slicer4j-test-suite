dfd386ba1ed6fac926e9b809c81ecbf0aa764deb
pshah <pshah5000@gmail.com>
2022-02-10 16:33:57 -0800

    Added try/catch block to main

--- Minepump/src/main/java/tests/tests.java
+++ Minepump/src/main/java/tests/tests.java
@@ -7,16 +7,22 @@ import MinePumpSystem.MinePump;
 public class tests {
     //RunningExample: [STARTCOMMAND___] [STOPCOMMAND___] [METHANEALARM___] [METHANEQUERY___] [LOWWATERSENSOR___] [HIGHWATERSENSOR___] BASE___ :: Program;
     public static void main(String args[]) {
-        System.out.println("Start");
-        Environment env = new Environment();
-        MinePump p = new MinePump(env);
-        env.waterRise();
-        env.waterRise();
-        p.timeShift();
-        env.waterRise();
-        p.timeShift();
-        p.timeShift();
-        p.timeShift();
-        System.out.println(p.toString());
+        try {
+            System.out.println("Start");
+            Environment env = new Environment();
+            MinePump p = new MinePump(env);
+            env.waterRise();
+            env.waterRise();
+            p.timeShift();
+            env.waterRise();
+            p.timeShift();
+            p.timeShift();
+            p.timeShift();
+            System.out.println(p.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Tests Failed!");
+        }
+
     }
 }
